dfd70875a677ef87794eb8ba545bb48008f16a8c
Natalia Castro <natliaacastroo@gmail.com>
2021-08-10 19:05:57 -0300

    layout

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logo from '../../../public/images/logo.svg';
+import { SignInButton } from '../SignInButton';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logo} alt='' />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import avatar from '../../public/images/avatar.svg';
+import { SubscribeButton } from '../Components/SubscribeButton';
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <Image src={avatar} alt='Girl coding' />
+      </main>
+    </>
+  );
+}
